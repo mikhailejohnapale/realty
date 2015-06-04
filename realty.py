f2e4b15f76becfa7a2b00a62272913a6fbe1c57e
Jan Maghuyop <jangmaghuyop@gmail.com>
2015-06-04 16:59:52 +0800

    ignored pycache, changed auto date to parameters for testing

--- realty.py
+++ realty.py
@@ -1,39 +1,34 @@
-from datetime import datetime
-
-
 BASIC_TAX_RATE = 0.011
 SEF_RATE = 0.01
 SH_RATE = 0.5
 
 
-def compute_discount_rate():
-    now = datetime.now()
-    if now.month == 1:
+def compute_discount_rate(month):
+    if month == 1:
         discount_rate = 0.2
-    elif now.month <= 3:
+    elif month <= 3:
         discount_rate = 0.1
     else:
         discount_rate = 0
     return discount_rate
 
 
-def compute_penalty_rate(date):
+def compute_penalty_rate(curr_date, prev_date):
     penalty_rate = 0
-    now = datetime.now()
-    if now.month > 3:
-        for year in range(now.year, date.year - 1, -1):
+    if curr_date.month > 3:
+        for year in range(curr_date.year, prev_date.year - 1, -1):
             # current year
-            if year == now.year:
-                if now.year == date.year:
-                    for month in range(1, now.month + 1):
+            if year == curr_date.year:
+                if curr_date.year == prev_date.year:
+                    for month in range(1, curr_date.month + 1):
                         penalty_rate += 2
                     break
                 else:
                     for month in range(1, 12 + 1):
                         penalty_rate += 2
             # previous year
-            elif year == date.year:
-                for month in range(1, date.month + 1):
+            elif year == prev_date.year:
+                for month in range(1, prev_date.month + 1):
                     penalty_rate += 2
             # middle years
             else:
@@ -52,6 +47,6 @@ def compute_sh_tax(assessed_value, type):
     return sh
 
 
-def compute_discount(tax):
-    discount_rate = compute_discount_rate()
+def compute_discount(tax, month):
+    discount_rate = compute_discount_rate(month)
     return tax * discount_rate
